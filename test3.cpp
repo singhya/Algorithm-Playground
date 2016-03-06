9ddffa01d79b24c0cf478956c8cd55163b6b8962
singhya <yash.minesweeper@gmail.com>
2016-03-05 19:33:10 -0800

    Clean up

--- test3.cpp
+++
@@ -1,6 +0,0 @@
-#include<iostream>
-int main()
-{
-	bool a[10000000];
-	return 0;
-}
--- testaray.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-	int a[250000][250000];
-	return 0;
-	
-}
